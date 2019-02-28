06639dc0b71aa4a1335a9f37e0e9a3e0c2ef2f93
justinkaufman <justinkaufman@justins-mbp-2.sduhsd.lan>
2019-02-28 10:10:43 -0800

    Big House Done

--- src/GraphicsUnit1/BigHouse.java
+++ src/GraphicsUnit1/BigHouse.java
@@ -32,19 +32,26 @@ public class BigHouse extends Canvas
       window.setColor(Color.BLUE);
 
       window.drawString( "BIG HOUSE ", 50, 50 );
-
+      //house
       window.setColor(Color.BLUE);
 
       window.fillRect( 200, 200, 400, 400 );
-      
+      //roof 
       window.setColor(Color.BLACK);
 
       window.fillRect( 170, 150, 450, 80 );
+      //windows
+      window.setColor(Color.YELLOW);
+
+      window.fillRect( 250, 300, 80, 80 );
       
       window.setColor(Color.YELLOW);
 
-      window.fillRect( 370, 350, 50, 50 );
-      
+      window.fillRect( 480, 300, 80, 80 );
+      //door
+      window.setColor(Color.WHITE);
+
+      window.fillRect( 365, 450, 80, 150 );
       
 
    }
